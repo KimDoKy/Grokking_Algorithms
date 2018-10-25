fcfea9f838c09674c7a2f29d480e2c45d7147928
KimDoKy <makingfunk0@gmail.com>
2018-10-25 09:06:26 +0900

    modified array -> binary search, mv directory

--- 01_introduction_to_algorithms/binary_search.py
+++ 01_introduction_to_algorithms/binary_search.py
@@ -2,7 +2,7 @@ def binary_search(list, item):
     low = 0
     high = len(list) - 1
     while low <= high:
-        mid = int((low + high) / 2)
+        mid = (low + high) // 2 
         guess = list[mid]
         if guess == item:
             return mid
